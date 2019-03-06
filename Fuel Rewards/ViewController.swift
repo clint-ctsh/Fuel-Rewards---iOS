46a2f34a5e70c45f5e705dad65844a4384d4b716
Jared Owens <jowens.software@gmail.com>
2019-03-06 11:55:43 -0600

    Initial Commit

---
+++ Fuel Rewards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fuel Rewards
+//
+//  Created by Jared on 3/6/19.
+//  Copyright © 2019 Cognizant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
