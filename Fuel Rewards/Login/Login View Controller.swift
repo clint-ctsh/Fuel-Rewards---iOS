7ec0bff182e6e4c14bcafe8c9d22c1e719af1270
Jared Owens <jowens.software@gmail.com>
2019-03-07 11:27:09 -0600

    Structured app more

---
+++ Fuel Rewards/Login/Login View Controller.swift
@@ -0,0 +1,9 @@
+//
+//  Login View Controller.swift
+//  Fuel Rewards
+//
+//  Created by Jared on 3/7/19.
+//  Copyright © 2019 Cognizant. All rights reserved.
+//
+
+import Foundation
--- Fuel Rewards/ViewController.swift
+++ Fuel Rewards/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var LoginPageBtnOutlet: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
